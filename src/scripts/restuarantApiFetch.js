0504c22c7719ac91d2885fdf25b80501271fe809
James Chapman <chapmanjamesg@gmail.com>
2019-10-21 15:27:42 -0500

    about to try to figure out if this will connect right

--- src/scripts/restuarantApiFetch.js
+++ src/scripts/restuarantApiFetch.js
@@ -1,13 +1,17 @@
 const zomatoApiBaseURL = 'https://developers.zomato.com/api/v2.1'
 
-const searchRestaurant = searchInput => fetch(`${zomatoApiBaseURL}/search?entity_id=1138&entity_type=city&${searchInput}`, 
-
-{'header':{
-    "accept": "application/json",
-    "user-key": '9fecd4ad44dabd6b0ef78a23c0491e3e'  
-}})
-.then(response => response.json())
-.then(response=> {
+const searchRestaurant = searchInput => fetch(`${zomatoApiBaseURL}/search?entity_id=1138&entity_type=city&${searchInput}`,
+    //this header is there for accessing the credentials you have to have a header object
+    {
+        'header': {
+            "accept": "application/json",
+            //calling the restaurant api key from the apiKeys.js that is currently in .gitignore
+            "user-key": `${restarauntsApi}`
+        }
+    }
+)
+    .then(response => response.json())
+    .then(response => {
         console.log(response.restaurant.name)
-        displayHtml(restaurantResponse.restaurant.name, restaurantResponse.restaurant.location.address, restaurantResponse.restaurant.cuisines)
-})
+        displayHtml(restaurantResponse.restaurant.name, restaurantResponse.restaurant.location.address, restaurantResponse.restaurant.phone_numbers)
+    })
