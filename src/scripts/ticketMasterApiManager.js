fba48a6729ca42b615274d1117845e86024a7a24
Trey Suiter <trey.suiter@gmail.com>
2019-10-21 13:57:22 -0500

    created TM api mngr

---
+++ src/scripts/ticketMasterApiManager.js
@@ -0,0 +1,4 @@
+const ticketMasterApiBaseUrl = "https://app.ticketmaster.com/discovery/v2/"
+
+const searchConcerts = searchInput => fetch(`${ticketMasterApiBaseUrl}events.json?classificationName=music&dmaId=324&keyword=${searchInput}&apikey=${ticketMasterApi}`)
+    .then(response => response.json())
